c6b791ceb895110db9dd829d1167946d7ef69c8c
Joanne Chen <joanne_r_chen@yahoo.com>
2020-06-02 13:53:50 +0100

    Step 8 Exercise 5

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,22 +1,80 @@
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
-x = 0
-while x <= students.length - 1 do
-puts "#{students[x][:name]} (#{students[x][:cohort]} cohort)"
-  x += 1
-end
+# students = [
+# {name: "Dr. Hannibal Lecter", cohort: :november},
+# {name: "Darth Vader", cohort: :november},
+# {name: "Nurse Ratched", cohort: :november},
+# {name: "Michael Corleone", cohort: :november},
+# {name: "Alex DeLarge", cohort: :november},
+# {name: "The Wicked Witch of the West", cohort: :november},
+# {name: "Terminator", cohort: :november},
+# {name: "Freddy Krueger", cohort: :november},
+# {name: "The Joker", cohort: :november},
+# {name: "Joffrey Baratheon", cohort: :november},
+# {name: "Norman Bates", cohort: :november}
+# ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student has to the array
+    students << {name: name, cohort: :november, age: :"25", hobbies: :tennis}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+def print(students)
+  students.each do | student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)
+      #{student[:hobby]} #{student[:age]}"
+  end
+end
+# I am not using bash but this code should works for the TL;DR s
+# ection: def ask prompt, default = nil; unless default;
+# return gets.chomp; end; return default; end; and you can use it
+# like: ask 'what is your name' waits for your reply. ask 'what
+# is your name', 'darek' this just return default value. Well, if
+# that's code that you are looking for I will post it as answer
+# and explain more (because comment section has character limits).
+# ps. or little hackish way: def ask2 prompt, default = nil;
+# default || gets.chomp; end;
+
+# def input_students
+#   # default = nil
+#   students = []
+#   puts "Please enter the names of the students"
+#   name = gets.chomp
+#   puts "Please enter the month of your cohort"
+#   month = gets.chomp
+#   puts "To finish, just hit return twice"
+#   while !name.empty? do
+#     # add the student has to the array
+#     students << {name: name, cohort: month}
+#     puts "Now we have #{students.count} students"
+#     name = gets.chomp
+#     month = gets.chomp
+#   end
+  # combine two arrays into a hash
+  # while the name is not empty, repeat this code
+  # while !name.empty? do
+    # add the student has to the array
+  # return the array of students
+#   students
+# end
+# input_students
+# def print(students)
+#   students.each do | student |
+#     puts "#{student[:name]} (#{student[:cohort]} cohort)"
+#   end
+# end
+# input_students
+# print
 # def input_students
 #   puts "Please enter the names of the students"
 #   puts "To finish, just hit return twice"
