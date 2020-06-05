7e3b4995814f9ad2a06d5ac2d21c2ba05d4ab80a
Joanne Chen <joanne_r_chen@yahoo.com>
2020-06-05 15:14:42 +0100

    Step 8: 9

---
+++ directory_exercise9.rb
@@ -0,0 +1,62 @@
+# Right now if we have only one student, the user
+# will see a message "Now we have 1 students", whereas
+# it should be "Now we have 1 student". How can you fix
+# it so that it used singular form when appropriate and
+# plural form otherwise?
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student has to the array
+    students << {name: name, cohort: :november, hobby: :war, age: :unknown}
+    if students.count < 2
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+# let's put all students into an array
+students = [
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
+]
+def print_header
+  puts "The students of my cohort at Makers"
+  puts "-------------".center(30)
+end
+def print(students)
+  students.each do | student |
+    puts "#{student[:name]} (#{student[:cohort]} cohort)
+     (#{student[:hobby]}) (#{student[:age]})"
+  end
+end
+def print_footer(names)
+  if names.count < 2
+    puts "Overall, we have #{names.count} great students".chomp("s")
+  else
+    puts "Overall, we have #{names.count} great students"
+  end
+end
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
